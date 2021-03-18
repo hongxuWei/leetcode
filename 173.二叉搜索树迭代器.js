464555dd86c847cbe5fb491b61c00ca071bac888
hongxu.wei <hongxu.wei@outlook.com>
2021-03-18 18:21:34 +0800

    q173

---
+++ 173.二叉搜索树迭代器.js
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=173 lang=javascript
+ *
+ * [173] 二叉搜索树迭代器
+ *
+ * https://leetcode-cn.com/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (76.02%)
+ * Likes:    343
+ * Dislikes: 0
+ * Total Accepted:    39.6K
+ * Total Submissions: 52.1K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
+  '[[[7,3,15,null,null,9,20]],[],[],[],[],[],[],[],[],[]]'
+ *
+ * 实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+ * 
+ * 调用 next() 将返回二叉搜索树中的下一个最小的数。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // 返回 3
+ * iterator.next();    // 返回 7
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 9
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 15
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 20
+ * iterator.hasNext(); // 返回 false
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+  this.list = [];
+
+  const init = () => {
+    const stack = [];
+    let current = root;
+    while (stack.length > 0 || current !== null) {
+      if (current) {
+        stack.push(current);
+        current = current.left;
+      } else {
+        current = stack.pop();
+        this.list.push(current);
+        current = current.right;
+      }
+    }
+  }
+  init();
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+  return this.list.shift().val;
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+  return this.list.length > 0;
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+// @lc code=end
+
